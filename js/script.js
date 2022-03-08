8bdc64959c2733ed66c0d746fa3035596b21503b
nolja <nolja30@naver.com>
2022-03-08 10:31:01 +0900

    가방슬라이더

--- js/script.js
+++ js/script.js
@@ -1,46 +1,32 @@
 $(document).ready(function(){
   $('.banner-slider').slick({
-     infinite: true ,      // 무한반복
+     infinite: true ,      
     centerMode: false ,
-     slidesToShow: 1,     // 보여지는 슬라이드 개수
-     slidesToScroll: 1,   // 넘어가는 슬라이드 개수
-     dots: true,         // 점 네비게이션 표시
-     arrows: true ,     // 화살표 표시
-     fade: false,            // 페이드 효과
-     vertical: false,         // 상하 슬라이드
-     autoplay: true,       //자동스크롤
-     autoplaySpeed: 1000 ,  //자동스크롤 속도
-    pauseOnHover: true, // 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
-    // responsive: 
-    // [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: true
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1
-    //     }
-    //   }
-    // ]
-    
-    
-    
-    
-    
+     slidesToShow: 1,     
+     slidesToScroll: 1,   
+     dots: true,        
+     arrows: true ,     
+     fade: false,         
+     vertical: false,         
+     autoplay: true,       
+     autoplaySpeed:2000 ,  
+    pauseOnHover: true, 
+  });
+});
+
+
+$(document).ready(function(){
+  $('.bag-slider').slick({
+     infinite: true ,      
+    centerMode: false ,
+     slidesToShow: 1,     
+     slidesToScroll: 1,   
+            
+     arrows: true ,     
+     fade: false,         
+     vertical: false,         
+     autoplay: true,       
+     autoplaySpeed:2000 ,  
+    pauseOnHover: true, 
   });
 });
